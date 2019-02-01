9cb578145a9d0827ccd470366f5dc4c6bca1c81b
Paulo Diniz <paulo.diniz@emphasysgroup.com.br>
2019-01-31 23:22:54 -0200

    Alteração nas classes base

---
+++ queries.js
@@ -0,0 +1,115 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+//const { Client } = require('pg-promise');
+
+var pgp = require('pg-promise')(options);
+
+
+var connectionString = 'postgres://postgres:Futurama2000@localhost:5432/financeira_db';
+// const connectionString = new Client({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'financeira_db',
+//     password: 'Futurama2000',
+//     port: 5432,
+//   });
+
+var db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+  getAllPuppies: getAllPuppies,
+  getSinglePuppy: getSinglePuppy,
+  createPuppy: createPuppy,
+  updatePuppy: updatePuppy,
+  removePuppy: removePuppy
+};
+
+function getAllPuppies(req, res, next) {
+    db.any('SELECT * FROM public.block')
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved ALL puppies'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+  
+  function getSinglePuppy(req, res, next) {
+    var pupID = parseInt(req.params.id);
+    db.one('SELECT * FROM public.block where id = $1', pupID)
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved ONE puppy'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+  function createPuppy(req, res, next) {
+    req.body.age = parseInt(req.body.age);
+    db.none('insert into pups(name, breed, age, sex)' +
+        'values(${name}, ${breed}, ${age}, ${sex})',
+      req.body)
+      .then(function () {
+        res.status(200)
+          .json({
+            status: 'success',
+            message: 'Inserted one puppy'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+  function updatePuppy(req, res, next) {
+    db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+      [req.body.name, req.body.breed, parseInt(req.body.age),
+        req.body.sex, parseInt(req.params.id)])
+      .then(function () {
+        res.status(200)
+          .json({
+            status: 'success',
+            message: 'Updated puppy'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+  function removePuppy(req, res, next) {
+    var pupID = parseInt(req.params.id);
+    db.result('delete from public.block where id = $1', pupID)
+      .then(function (result) {
+        /* jshint ignore:start */
+        res.status(200)
+          .json({
+            status: 'success',
+            message: `Removed ${result.rowCount} puppy`
+          });
+        /* jshint ignore:end */
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+
+var db = require('../queries');
+
+
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
